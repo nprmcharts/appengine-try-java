d930447c690370775d8f6fbfc8a914215e8bdf09
Sarah <nprmcharts@gmail.com>
2020-03-31 19:55:33 -0400

    BigQuery sample
    https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -28,13 +28,63 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.net.URL;
 import org.apache.commons.io.FileUtils;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.FieldValueList;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.TableResult;
+import java.util.UUID;
 
-public class DemoServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
+public class DemoServlet {
+  public static void main(String... args) throws Exception {
+    // [START bigquery_simple_app_client]
+    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+    // [END bigquery_simple_app_client]
+    // [START bigquery_simple_app_query]
+    QueryJobConfiguration queryConfig =
+        QueryJobConfiguration.newBuilder(
+                "SELECT "
+                    + "CONCAT('https://stackoverflow.com/questions/', CAST(id as STRING)) as url, "
+                    + "view_count "
+                    + "FROM `bigquery-public-data.stackoverflow.posts_questions` "
+                    + "WHERE tags like '%google-bigquery%' "
+                    + "ORDER BY favorite_count DESC LIMIT 10")
+            // Use standard SQL syntax for queries.
+            // See: https://cloud.google.com/bigquery/sql-reference/
+            .setUseLegacySql(false)
+            .build();
+
+    // Create a job ID so that we can safely retry.
+    JobId jobId = JobId.of(UUID.randomUUID().toString());
+    Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+
+    // Wait for the query to complete.
+    queryJob = queryJob.waitFor();
+
+    // Check for errors
+    if (queryJob == null) {
+      throw new RuntimeException("Job no longer exists");
+    } else if (queryJob.getStatus().getError() != null) {
+      // You can also look at queryJob.getStatus().getExecutionErrors() for all
+      // errors, not just the latest one.
+      throw new RuntimeException(queryJob.getStatus().getError().toString());
+    }
+    // [END bigquery_simple_app_query]
+
+    // [START bigquery_simple_app_print]
+    // Get the results.
+    TableResult result = queryJob.getQueryResults();
+
+    // Print all pages of the results.
+    for (FieldValueList row : result.iterateAll()) {
+      String url = row.get("url").getStringValue();
+      long viewCount = row.get("view_count").getLongValue();
+      System.out.printf("url: %s views: %d%n", url, viewCount);
+    }
+    // [END bigquery_simple_app_print]
   }
-
 }
+// [END bigquery_simple_app_all]
